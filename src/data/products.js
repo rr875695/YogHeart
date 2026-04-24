c60c8a489688a03277eee560f3f2e8098c64951a
rr875695 <122020904+rr875695@users.noreply.github.com>
2026-04-25 00:07:41 +0530

    Refactor product images to use imports

--- src/data/products.js
+++ src/data/products.js
@@ -1,6 +1,11 @@
+import yogurt from "../components/yogurt.png";
+import tablet from "../components/tablet.png";
+import capsule from "../components/capsule.png";
+import starter from "../components/starter.png";
+
 export const products = [
-  { id:1, name:"Yogurt", price:499, benefit:"Better digestion & immunity", image:"../components/yogurt.png" },
-  { id:2, name:"Chewable Tablet", price:699, benefit:"Dental Health", image:"/src/components/tablet.png" },
-  { id:3, name:"Capsule", price:899, benefit:"Improves stamina & focus", image:"/src/components/capsule.png" },
-  { id:4, name:"Yogurt Starter", price:299, benefit:"Fermented Dairy Starter Culture", image:"/src/components/starter.png" }
+  { id:1, name:"Yogurt", price:499, benefit:"Better digestion & immunity", image: yogurt },
+  { id:2, name:"Chewable Tablet", price:699, benefit:"Dental Health", image: tablet },
+  { id:3, name:"Capsule", price:899, benefit:"Improves stamina & focus", image: capsule },
+  { id:4, name:"Yogurt Starter", price:299, benefit:"Fermented Dairy Starter Culture", image: starter }
 ];
